c439814b6a0b9bfeb906fe9f7e6a66c89eeffccd
Efraín Manosalvas <angel.manosalvas@gmail.com>
2022-05-18 00:08:50 -0500

    WIP - working on ViewHolder for recycler view of products.

--- app/android/app/src/main/java/com/example/shoppinglist/shoppingListApp/ui/products/ProductsModel.kt
+++ app/android/app/src/main/java/com/example/shoppinglist/shoppingListApp/ui/products/ProductsModel.kt
@@ -2,4 +2,12 @@ package com.example.shoppinglist.shoppingListApp.ui.products
 
 data class ProductsModel(
 
+    val pictureUrl: String,
+    val title: String,
+    val category: String,
+    val provider: String,
+    val price: Int,
+    val qualification: Int,
+    val observations: String
+
 )
--- app/android/app/src/main/java/com/example/shoppinglist/shoppingListApp/ui/products/ProductsViewHolder.kt
+++ app/android/app/src/main/java/com/example/shoppinglist/shoppingListApp/ui/products/ProductsViewHolder.kt
@@ -1,4 +1,28 @@
 package com.example.shoppinglist.shoppingListApp.ui.products
 
-class ProductsViewHolder {
+import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+
+class ProductsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val productImage = view.findViewById(R.id.ProductImage) as ImageView
+    val productTitle = view.findViewById(R.id.ProductTitle) as TextView
+    val productCategory = view.findViewById(R.id.ProductCategory) as TextView
+    val productProvider = view.findViewById(R.id.ProductProvider) as TextView
+    val productPrice = view.findViewById(R.id.ProductPrice) as TextView
+    val scoreStar1 = view.findViewById(R.id.ProductQualityStar1) as ImageView
+    val scoreStar2 = view.findViewById(R.id.ProductQualityStar2) as ImageView
+    val scoreStar3 = view.findViewById(R.id.ProductQualityStar3) as ImageView
+    val scoreStar4 = view.findViewById(R.id.ProductQualityStar4) as ImageView
+    val scoreStar5 = view.findViewById(R.id.ProductQualityStar5) as ImageView
+    val productObservation = view.findViewById(R.id.ProductObservations) as TextView
+
+    fun bind (product: ProductsModel, context: Context){
+        productTitle.text = product.title
+    }
+
 }
